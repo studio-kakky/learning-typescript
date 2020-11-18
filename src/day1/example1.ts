6f8a2ceaa62ef7d21810bfe328a5b56b7753da52
柿内暢昌 <nob@studio-kakky.com>
2020-11-18 11:55:02 +0900

    day1

---
+++ src/day1/example1.ts
@@ -0,0 +1,5 @@
+(() => {
+  let helloWorld = 'hello world';
+})()
+
+
---
+++ src/day1/example2.ts
@@ -0,0 +1,19 @@
+(()=> {
+  let userName: string;
+  userName = 'name';
+
+  interface User {
+    name: string;
+    id: number;
+  }
+
+  const user: User = {
+    name: 'name',
+    id: 1
+  }
+
+  const user2: User = {
+    name: 'name',
+    id: 1
+  }
+})()
